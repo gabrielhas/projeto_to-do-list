8c56fd7299f56216f1d59e909eed81c2a9624e92
Gabriel <gabrielbcv@outlook.com>
2026-03-27 15:18:46 -0300

    Adicionar e Excluir do BD

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import EditTask from "@/components/editTask";
-import DeleteTask from "@/components/deleteTask";
 
 import {
   List,
@@ -32,17 +31,58 @@ import {
 import { getTasks } from "@/actions/get-tasks-from-db";
 import { useEffect, useState } from "react";
 import { Tasks } from "@/src/generated/prisma/client";
+import { NewTask } from "@/actions/add-task-from-db";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogTitle,
+  DialogContent,
+  DialogHeader,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { deleteTask } from "@/actions/delete-task-from-db";
 
 export default function Home() {
   const [tastList, setTaskList] = useState<Tasks[]>([]);
-  //const [task, setTask] = useState<string>("");
+  const [task, setTask] = useState<string>("");
 
   const handledGetTasks = async () => {
-    const tasks = await getTasks();
+    try {
+      const tasks = await getTasks();
+      if (tasks) {
+        setTaskList(tasks);
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    if (!tasks) return;
+  const handleAddTask = async () => {
+    if (!task || task.trim().length === 0) return;
 
-    setTaskList(tasks);
+    try {
+      const newTask = await NewTask(task);
+
+      if (!newTask) return;
+
+      setTaskList([...tastList, newTask]);
+      setTask(""); // Clear input state
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const handleDeleteTask = async (taskId: number) => {
+    try {
+      if (!taskId) return;
+
+      const deletedTask = await deleteTask(taskId);
+
+      if (!deletedTask) return;
+      handledGetTasks();
+    } catch (error) {
+      throw error;
+    }
   };
 
   useEffect(() => {
@@ -53,11 +93,16 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen bg-gray-200">
       <Card className="w-lg">
         <CardHeader className="flex gap-2 items-center">
-          <Input placeholder="Digite sua tarefa" />
+          <Input
+            placeholder="Digite sua tarefa"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
           <Button
             variant="default"
             size="sm"
             className="cursor-pointer font-serif"
+            onClick={handleAddTask}
           >
             Adicionar
             <Plus />
@@ -101,7 +146,39 @@ export default function Home() {
 
                 <div className="flex items-center gap-2">
                   <EditTask />
-                  <DeleteTask />
+
+                  <Dialog>
+                    <DialogTrigger>
+                      <Trash2 className="cursor-pointer" size={16} />
+                    </DialogTrigger>
+
+                    <DialogContent className="w-65">
+                      <DialogHeader>
+                        <DialogTitle className="text-center">
+                          Deseja excluir essa tarefa?
+                        </DialogTitle>
+                      </DialogHeader>
+                      <div className="flex justify-center gap-2">
+                        <Button
+                          variant="default"
+                          className="cursor-pointer"
+                          onClick={() => handleDeleteTask(task.id)}
+                        >
+                          Sim
+                        </Button>
+                        <DialogClose
+                          render={
+                            <Button
+                              variant="outline"
+                              className="cursor-pointer"
+                            >
+                              Não
+                            </Button>
+                          }
+                        />
+                      </div>
+                    </DialogContent>
+                  </Dialog>
                 </div>
               </div>
             ))}
